778e4695d279db678fec49e8b4225d430280b6e4
toddhass <toddhassinger@icloud.com>
2026-09-13 12:33:53 -0400

    Practice page

---
+++ src/pages/PracticePage.tsx
@@ -0,0 +1,9 @@
+import { PracticePanel } from "../components/PracticePanel";
+
+export function PracticePage() {
+  return (
+    <div className="page page-practice">
+      <PracticePanel />
+    </div>
+  );
+}
